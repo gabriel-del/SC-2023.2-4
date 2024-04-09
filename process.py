f3b5a29e4760af506bc6983192eaf2d6c1f51a6b
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-04-09 18:44:48 -0300

    adjust country graph

--- process.py
+++ process.py
@@ -20,20 +20,7 @@ df = df.dropna(subset=["country"])
 
 df["meal"].replace("Undefined", "SC", inplace=True)
 
-month_mapping = {
-    'January': 1,
-    'February': 2,
-    'March': 3,
-    'April': 4,
-    'May': 5,
-    'June': 6,
-    'July': 7,
-    'August': 8,
-    'September': 9,
-    'October': 10,
-    'November': 11,
-    'December': 12
-}
+month_mapping = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
 
 df['arrival_date_month'] = df['arrival_date_month'].map(month_mapping)
 
