526cf4925920a96c4082a9d80f33b113c99d7a63
in_kenzo <kenzoinanami@gmail.com>
2024-04-12 20:09:18 -0300

    Adicionado mais descrições

--- process.py
+++ process.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('hotel_bookings.csv')
 
-# Rm Children null
+# Para o número de crianças na reserva do quarto, preenchemos esse valor como se fosse 0, 
+# pois é o valor mais frequente.
 df['children'].fillna(0, inplace=True)
 df = df.astype({'children': 'int64'})
 
@@ -11,13 +12,20 @@ df = df.astype({'children': 'int64'})
 df['total_guests'] = df['adults'] + df['children'] + df['babies']
 
 # Rm Total Guests = 0
+# Apagamos as linhas de reservas que possuiam 0 adultos, 0 crianças e 0 bebês, 
+# pois não é possível haver uma reserva em casos como esse.
 df = df[df["total_guests"] != 0]
 
+# Quase todos os campos 'company' do conjunto estavam vazios, 
+# então decidimos apagar essa coluna pois não poderíamos utilizar.
 df = df.drop('company',axis=1)
 
 # Rm country null
+# Já no caso de country, como são menos valores nulos, decidimos apagar as tuplas com esse campo nulo.
 df = df.dropna(subset=["country"])
 
+# Na descrição do conjunto é especificado que quando 'meal' é igual a 'Undefined' 
+# isso é igual a quando está marcado como 'SC', por isso decidimos uniformizar isso.
 df["meal"].replace("Undefined", "SC", inplace=True)
 
 month_mapping = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
